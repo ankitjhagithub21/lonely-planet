5793a64ecb1754c21457d0fadb4bef1eabc83c96
ankitjhagithub21 <ajha45075@gmail.com>
2025-02-07 19:27:56 +0530

    minor chanages

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -11,8 +11,8 @@ const VideoSection = () => {
 
             </video>
            <div className="overlay  z-10 relative top-0 left-0 w-full h-full flex flex-col  gap-5 items-center justify-center">
-           <h1 className='font-serif text-9xl text-center  text-white'>Best in Travel</h1>
-           <h2 className='text-6xl text-white'>2025</h2>
+           <h1 className='font-serif lg:text-9xl text-5xl text-center  text-white'>Best in Travel</h1>
+           <h2 className='lg:text-6xl text-3xl text-white'>2025</h2>
            <button className='px-6 py-2 text-sm bg-indigo-700 text-white rounded-full hover:bg-indigo-800'>Discover the winners</button>
            </div>
         </section>
